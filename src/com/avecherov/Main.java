86918f5a62249bcaa717c4fae50702aa1a77c218
Artyom Vecherov <a.vecherov@gmail.com>
2018-03-24 02:36:54 +0300

    Initial commit

--- src/com/avecherov/Main.java
+++ src/com/avecherov/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println(12.00 % 1);
+        System.out.println(((2<2) && (3>=3)));
     }
 }
--- src/com/avecherov/SolutionTest.java
+++ src/com/avecherov/SolutionTest.java
@@ -1,37 +1,21 @@
 package com.avecherov;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class SolutionTest {
 
-    private Factorial fact;
-
-    @Before
-    public void initFactorial() {
-        fact = new Factorial();
-    }
-
-    @After
-    public void afterFactorial() {
-        fact = null;
-    }
-
     @Test
-    public void test_factorial0() {
-        assertEquals(1, fact.factorial(0));
-    }
-
-    @Test
-    public void test_factorial3() {
-        assertEquals(6, fact.factorial(3));
-    }
-
-    @Test
-    public void test_factorial5() {
-        assertEquals(120, fact.factorial(5));
+    public void testSample() {
+        People person = People.builder().name("Adam").lastName("Savage").age(25).city("San Francisco").job("Unchained Reaction").build();
+        assertEquals(
+                "Adam",
+                person.getName()
+        );
+        assertEquals(
+                "hello my name is Adam",
+                person.greet()
+        );
     }
 }
